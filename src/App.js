a3afc7ee66c88b35e98e57a6699ec5a78c5e78ed
Yael Sheinman <yael.sheinman@gmail.com>
2024-01-26 19:17:21 +0000

    create book page and some styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 
 import Header from "./components/Header/Header";
 import Homepage from "./pages/Homepage/Homepage";
+import SingleBook from "./pages/Book/Book";
 
 function App() {
   return (
@@ -11,13 +12,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/homepage" element={<Homepage />} />
+        <Route path="/book/:id" element={<SingleBook />} />
         {/* 
         <Route path="/profile" element={<Profile />} />
         <Route path="/list" element={<List />} />
         <Route path="/list/read" element={<ReadList />} />
         <Route path="/list/recommendations" element={<RecommendationsList />} />
-        <Route path="/book/:id" element={<SingleBook />} />
-        <Route path="/questionnaire" element={<Questionnaire />} /> */}
+      <Route path="/questionnaire" element={<Questionnaire />} /> */}
 
         <Route path="/*" element={<div>page not found</div>} />
       </Routes>
