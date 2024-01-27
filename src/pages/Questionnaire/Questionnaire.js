38d431bd737e91fe4b7bb300f8fb68ca6d0af777
Yael Sheinman <yael.sheinman@gmail.com>
2024-01-27 23:38:24 +0000

    add some styling and try to make length filter

--- src/pages/Questionnaire/Questionnaire.js
+++ src/pages/Questionnaire/Questionnaire.js
@@ -1,3 +1,5 @@
+import "./Questionnaire.scss";
+
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -13,7 +15,7 @@ const Questionnaire = () => {
   });
   const [listAllBooks, setListAllBooks] = useState([]);
   const [menuTheme, setMenuTheme] = useState([]);
-  const [length, setLength] = useState([]);
+  const [bookLength, setBookLength] = useState([]);
 
   const handleAnswer = (event) => {
     const { name, value } = event.target;
@@ -34,11 +36,11 @@ const Questionnaire = () => {
     fetchThemes();
   }, []);
 
-  //   filter through questions
+  //   filter through questions for themes
   useEffect(() => {
-    console.log(answer.question1);
-    console.log(answer.question2);
-    const filteredBooks = listAllBooks.filter((book) => {
+    // console.log(answer.question1);
+    // console.log(answer.question2);
+    const filteredBooksTheme = listAllBooks.filter((book) => {
       if (answer.question1 == "fiction" && book.fiction) {
         return book;
       } else if (answer.question1 == "non-fiction" && !book.fiction) {
@@ -46,15 +48,53 @@ const Questionnaire = () => {
       }
     });
     const themes = new Array(
-      ...new Set(filteredBooks.map((book) => book.themes).flat())
+      ...new Set(filteredBooksTheme.map((book) => book.themes).flat())
     );
+    // console.log(themes);
 
     setMenuTheme(themes);
   }, [answer.question1]);
 
+  // filter through questions for pages
   useEffect(() => {
-    console.log(answer.question3);
-  }, [answer.question3]);
+    // console.log(answer.question3);
+
+    const filteredBooksLength = listAllBooks.filter((book) => {
+      const isUnder500 =
+        answer.question1 && answer.question2 && book.pages.data < 500;
+      const isBetween500And800 = book.pages.data > 500 && book.pages.data < 800;
+      const isAbove800 =
+        answer.question1 && answer.question2 && book.pages.data > 800;
+
+      const isFilterValid = () => {
+        if (isUnder500 || isBetween500And800 || isAbove800) {
+          return true;
+        }
+      };
+
+      if (isUnder500) {
+        return "small";
+      }
+      if (isBetween500And800) {
+        return "medium";
+      }
+      if (isAbove800) {
+        return "large";
+      }
+
+      return false;
+      isFilterValid();
+    });
+
+    const uniqueBookLengths = [
+      ...new Set(filteredBooksLength.map((book) => book.pages.data).flat()),
+    ];
+    // console.log(uniqueBookLengths);
+
+    setBookLength(uniqueBookLengths);
+
+    console.log(filteredBooksLength);
+  }, [answer.question1, answer.question2, answer.question3, listAllBooks]);
 
   const isFormValid = () => {
     if (!answer.question1 || !answer.question2) {
@@ -86,15 +126,20 @@ const Questionnaire = () => {
 
   return (
     <main>
-      <h1>wow what a crazy questionnaire</h1>
-      <p>wow what a good mini explanation</p>
-      <p>pick fiction or non fiction, then choose theme, length is optional</p>
+      <div className="intro">
+        <h1>wow what a crazy questionnaire</h1>
+        <p>wow what a good mini explanation</p>
+        <p>
+          pick fiction or non fiction, then choose theme, length is optional
+        </p>
+      </div>
 
       <form className="form" onSubmit={submitHandler}>
+        {/* question 1 */}
         <div className="form__container">
-          <p>question 1</p>
+          <p className="form__question">question 1</p>
 
-          <label htmlFor="question" className="form__question">
+          <label htmlFor="question" className="form__question-label">
             fiction
             <input
               type="radio"
@@ -104,7 +149,7 @@ const Questionnaire = () => {
               onChange={handleAnswer}
             />
           </label>
-          <label htmlFor="question" className="form__question">
+          <label htmlFor="question" className="form__question-label">
             non-fiction
             <input
               type="radio"
@@ -116,9 +161,10 @@ const Questionnaire = () => {
           </label>
         </div>
 
+        {/* question 2 */}
         <div className="form__container">
-          <p>question 2</p>
-          <label htmlFor="question" className="form__question">
+          <p className="form__question">question 2</p>
+          <label htmlFor="question" className="form__question-label">
             {" "}
             Please select{" "}
           </label>
@@ -127,6 +173,7 @@ const Questionnaire = () => {
             className="form__question-theme"
             onChange={handleAnswer}
           >
+            <option>select theme</option>
             {menuTheme?.length > 0 &&
               menuTheme.map((themeName) => {
                 return (
@@ -137,47 +184,37 @@ const Questionnaire = () => {
               })}
           </select>
         </div>
+
+        {/* question 3 */}
         <div className="form__container">
-          <p>question 3</p>
-          <label htmlFor="question" className="form__question">
-            small - less than 500 pages
-            <input
-              type="radio"
-              name="question3"
-              value="small"
-              className="form__question-input"
-              onChange={handleAnswer}
-            />
+          <p className="form__question">question 3</p>
+          <label htmlFor="question" className="form__question-label">
+            {" "}
+            Please select{" "}
           </label>
-          <label htmlFor="question" className="form__question">
-            medium - between 500 and 800 pages
-            <input
-              type="radio"
-              name="question3"
-              value="medium"
-              className="form__question-input"
-              onChange={handleAnswer}
-            />
-          </label>
-          <label htmlFor="question" className="form__question">
-            large - more than 800 pages
-            <input
-              type="radio"
-              name="question3"
-              value="large"
-              className="form__question-input"
-              onChange={handleAnswer}
-            />
-          </label>
+
+          <select
+            name="question3"
+            className="form__question-length"
+            onChange={handleAnswer}
+          >
+            <option value="0">select length</option>
+            {/* if isUnder500 */}
+            <option value="1">small</option>
+            {/* if isBetween500And800 */}
+            <option value="2">medium</option>
+            {/* if isAbove800 */}
+            <option value="3">large</option>
+          </select>
         </div>
 
-        <div className="form__button">
-          <Link to="/" className="form__button-cancel">
-            <p>Cancel</p>
-          </Link>
+        <div className="form__container form__button">
           {/* <Link to="/list/recommendations" className="form__button-submit"> */}
           <button className="form__button-submit">GET RECOMMENDATION!</button>
           {/* </Link> */}
+          <Link to="/" className="form__button-cancel">
+            <p>Cancel</p>
+          </Link>
         </div>
       </form>
     </main>
