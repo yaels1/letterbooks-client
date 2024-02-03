7948958041c307e5f6fd8b4690ecb0a90581036b
Yael Sheinman <yael.sheinman@gmail.com>
2024-02-03 22:29:20 +0000

    breakpoint styling for homepage and desktop for header

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -15,22 +15,25 @@ const HomePage = () => {
   };
 
   return (
-    <main>
+    <main className="homepage__main">
       <div className="homepage">
         <h1 className="homepage__title">Welcome to the homepage!</h1>
         <div className="homepage__intro">
           <div className="homepage__text">
-            <p>This is an book app</p>
-            <p>
-              There is a quick easy questionnaire to take, to figure out which
-              book will be your next adventure.
-            </p>
-            <p>
-              There are around 50 books in here so far, which you can find in
-              the lists section
-            </p>
-
-            <p>WISHLIST TO COME</p>
+            <div className="homepage__text-container">
+              <p>This is an book app</p>
+              <p>
+                There is a quick easy questionnaire to take, to figure out which
+                book will be your next adventure.
+              </p>
+            </div>
+            <div className="homepage__text-container">
+              <p>
+                There are around 50 books in here so far, which you can find in
+                the lists section
+              </p>
+              <p>WISHLIST TO COME</p>
+            </div>
           </div>
           <img
             src={cosyPhoto}
