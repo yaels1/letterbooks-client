f5e87c1c0fff17536ca89f2851188233140ed230
Yael Sheinman <yael.sheinman@gmail.com>
2024-02-03 20:50:39 +0000

    set up breakpoint mixins and variables and do header for all screen sizes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,29 +5,27 @@ import logo from "../../assets/logo/books-stack-of-three (1).png";
 
 const Header = () => {
   return (
-    <div>
-      <header className="header">
-        <div className="header__title">
-          <img src={logo} className="header__logo" alt="book stack logo" />
-          <h1 className="header__title-text">LETTERBOOKS</h1>
-        </div>
-        <nav className=" nav">
-          <NavLink to="/" className="nav__header">
-            HOME
-          </NavLink>
+    <div className="header">
+      <div className="header__title">
+        <img src={logo} className="header__logo" alt="book stack logo" />
+        <h1 className="header__title-text">LETTERBOOKS</h1>
+      </div>
+      <nav className=" nav">
+        <NavLink to="/" className="nav__header">
+          HOME
+        </NavLink>
 
-          <NavLink to="/profile" className="nav__header">
-            PROFILE
-          </NavLink>
-          <NavLink to="/list" className="nav__header">
-            LISTS
-          </NavLink>
+        <NavLink to="/profile" className="nav__header">
+          PROFILE
+        </NavLink>
+        <NavLink to="/list" className="nav__header">
+          LISTS
+        </NavLink>
 
-          <NavLink to="/questionnaire" className="nav__header">
-            QUESTIONNAIRE
-          </NavLink>
-        </nav>
-      </header>
+        <NavLink to="/questionnaire" className="nav__header">
+          QUESTIONNAIRE
+        </NavLink>
+      </nav>
     </div>
   );
 };
