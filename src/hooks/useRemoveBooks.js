3a36d91eb25d2738e1ad7fb4bad20ca382ad5c9d
Yael Sheinman <yael.sheinman@gmail.com>
2024-02-24 23:07:47 +0000

    add more to removebook custom hook

--- src/hooks/useRemoveBooks.js
+++ src/hooks/useRemoveBooks.js
@@ -3,15 +3,14 @@ import { useState, useEffect } from "react";
 
 const apiUrl = process.env.REACT_APP_API_URL + process.env.REACT_APP_API_PORT;
 
-const useRemoveBooks = () =>{
+const useRemoveBooks = () => {
+  const [removeBook, setRemoveBook] = usestate(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-    const [removeBook, setRemoveBook] = usestate(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-
-
-    const DeleteBook
-
-
-
-}
+  const DeleteBook = async () => {
+    const token = localStorage.getItem("tokenlogin");
+    const decoded = jwtDecode(token);
+    await axios.delete(`${apiUrl}/letterbooks/list/${decoded.id}/wishlist`);
+  };
+};
